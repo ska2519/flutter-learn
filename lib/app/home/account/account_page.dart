66d6a92460e887034e5a66a18af5c926a69d1637
Ska Lee <ska2519@gmail.com>
2021-05-28 12:55:35 +0900

    Update Sign in UI Interaction with Google Sign-in

--- lib/app/home/account/account_page.dart
+++ lib/app/home/account/account_page.dart
@@ -1,19 +1,22 @@
 import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_learn/app/sign_in/sign_in_page.dart';
 import 'package:flutter_learn/app/top_level_providers.dart';
 import 'package:flutter_learn/app/widgets/alert_dialogs/show_alert_dialog.dart';
 import 'package:flutter_learn/app/widgets/alert_dialogs/show_exception_alert_dialog.dart';
 import 'package:flutter_learn/app/widgets/avatar.dart';
+import 'package:flutter_learn/app/widgets/empty_content.dart';
 import 'package:flutter_learn/constants/constants.dart';
 import 'package:flutter_learn/constants/keys.dart';
 import 'package:flutter_learn/constants/strings.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pedantic/pedantic.dart';
 
-class AccountPage extends StatelessWidget {
+class AccountPage extends HookWidget {
   Future<void> _signOut(BuildContext context, FirebaseAuth firebaseAuth) async {
     try {
       await firebaseAuth.signOut();
@@ -44,32 +47,47 @@ class AccountPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final firebaseAuth = context.read(firebaseAuthProvider);
-    final user = firebaseAuth.currentUser;
-    print('user: $user');
-    return Scaffold(
-      appBar: AppBar(
-        actions: <Widget>[
-          TextButton(
-            key: user != null ? const Key(Keys.logout) : const Key(Keys.login),
-            onPressed: () => user != null
-                ? _confirmSignOut(context, firebaseAuth)
-                : Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => SignInPage(),
-                    )),
-            child: Text(
-              user != null ? Strings.logout : Strings.login,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.white,
+    final authStateChanges = useProvider(authStateChangesProvider);
+
+    return authStateChanges.when(
+      data: (user) => Scaffold(
+        appBar: AppBar(
+          actions: <Widget>[
+            TextButton(
+              key:
+                  user != null ? const Key(Keys.logout) : const Key(Keys.login),
+              onPressed: () => user != null
+                  ? _confirmSignOut(context, firebaseAuth)
+                  : Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SignInPage(),
+                      ),
+                    ),
+              child: Text(
+                user != null ? Strings.logout : Strings.login,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.white,
+                ),
               ),
             ),
+          ],
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(150.0),
+            child: _buildUserInfo(user),
           ),
-        ],
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(150.0),
-          child: _buildUserInfo(user),
+        ),
+      ),
+      loading: () => const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+      error: (_, __) => const Scaffold(
+        body: EmptyContent(
+          title: 'Something went wrong',
+          message: "Can't load data right now.",
         ),
       ),
     );
@@ -86,7 +104,7 @@ class AccountPage extends StatelessWidget {
         ),
         const SizedBox(height: defaultPadding),
         Text(
-          user?.displayName ?? 'Need to Login',
+          user?.displayName ?? '로그인이 필요합니다',
           style: const TextStyle(color: Colors.white),
         ),
         const SizedBox(height: defaultPadding),
