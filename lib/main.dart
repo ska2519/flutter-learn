3bae5e7911672dfea348be86f27da0a0adb6e171
Ska Lee <ska2519@gmail.com>
2021-06-03 20:40:16 +0900

    Setting up internationalized app w/ iOS setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
 import 'package:flutter_learn/app/home/home_page.dart';
 import 'package:flutter_learn/routes/app_router.dart';
 import 'package:flutter_learn/services/firebase_auth_service.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,6 +20,8 @@ class MyApp extends StatelessWidget {
     final auth = context.read(authServiceProvider);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      // onGenerateTitle: (BuildContext context) =>
+      //     DemoLocalizations.of(context).title,
       title: 'Flutter Learn',
       theme: ThemeData(
         brightness: Brightness.light,
@@ -27,9 +32,21 @@ class MyApp extends StatelessWidget {
           //titleTextStyle: TextStyle(color: Colors.white),
           iconTheme: IconThemeData(color: Colors.white),
         ),
-        textTheme: TextTheme(headline6: TextStyle(color: Colors.white)),
+        textTheme: TextTheme(
+          headline6: TextStyle(color: Colors.white),
+        ),
         fontFamily: 'NotoSansKR',
       ),
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''), // English, no country code
+        const Locale('ko', 'KR'),
+      ],
       home: HomePage(),
       onGenerateRoute: (settings) => AppRouter.onGenerateRoute(settings, auth),
     );
