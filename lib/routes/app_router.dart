ad32258615825d8890d6f5a3bae7df63f021f516
Ska Lee <ska2519@gmail.com>
2021-06-10 23:00:17 +0900

    update PostDetailPage

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_learn/app/home/account/account_page.dart';
+import 'package:flutter_learn/app/home/community/edit_post_page.dart';
+import 'package:flutter_learn/app/home/community/post_detail_page.dart';
 import 'package:flutter_learn/app/home/desktop/community_screen.dart';
 import 'package:flutter_learn/app/home/desktop/youtube_screen.dart';
 import 'package:flutter_learn/app/home/home_page.dart';
 import 'package:flutter_learn/app/sign_in/email/email_password_sign_in_page.dart';
 import 'package:flutter_learn/app/sign_in/sign_in_page.dart';
+import 'package:flutter_learn/models/post.dart';
 import 'package:flutter_learn/services/auth_base.dart';
 
 class AppRoutes {
   static const emailPasswordSignInPage = '/email-password-sign-in-page';
   static const signInPage = '/sign-in-page';
+  static const editPostPage = '/edit-post-page';
+  static const postDetailPage = '/post-detail-page';
   static const appPage = '/app-page';
   static const communityScreen = '/community-screen';
   static const youtubeScreen = '/youtube-screen';
@@ -37,7 +42,18 @@ class AppRouter {
           settings: settings,
           fullscreenDialog: true,
         );
-
+      case AppRoutes.editPostPage:
+        return MaterialPageRoute<dynamic>(
+          builder: (_) => EditPostPage(post: args as Post?),
+          settings: settings,
+          fullscreenDialog: true,
+        );
+      case AppRoutes.postDetailPage:
+        return MaterialPageRoute<dynamic>(
+          builder: (_) => PostDetailPage(post: args! as Post),
+          settings: settings,
+          fullscreenDialog: true,
+        );
       case AppRoutes.appPage:
         return MaterialPageRoute<dynamic>(
           builder: (_) => HomePage(),
