5cc409f002b11719ea81e377ec06569e0c2860b6
Ska Lee <ska2519@gmail.com>
2021-06-11 15:05:30 +0900

    add comment in FB Firestore

--- lib/services/firestore_path.dart
+++ lib/services/firestore_path.dart
@@ -4,7 +4,9 @@ class FirestorePath {
   static String entry(String uid, String entryId) =>
       'users/$uid/entries/$entryId';
   static String entries(String uid) => 'users/$uid/entries';
+  static String appUser(String id) => 'users/$id';
   static String post(String id) => 'posts/$id';
   static String posts() => 'posts';
-  static String appUser(String id) => 'users/$id';
+  static String comments() => 'comments';
+  static String comment(String postId) => '/posts/$postId/comments';
 }
