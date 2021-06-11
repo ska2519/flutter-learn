ff2b8f4e3853f42adf349110694917b3231dda40
Ska Lee <ska2519@gmail.com>
2021-06-11 23:04:43 +0900

    Models update with Set{}

--- lib/models/comment.g.dart
+++ lib/models/comment.g.dart
@@ -11,6 +11,9 @@ _$_Comment _$_$_CommentFromJson(Map json) {
     text: json['text'] as String,
     postId: json['postId'] as String,
     userId: json['userId'] as String,
+    timestamp: (json['timestamp'] as List<dynamic>?)
+        ?.map((e) => DateTime.parse(e as String))
+        .toSet(),
   );
 }
 
@@ -19,4 +22,5 @@ Map<String, dynamic> _$_$_CommentToJson(_$_Comment instance) =>
       'text': instance.text,
       'postId': instance.postId,
       'userId': instance.userId,
+      'timestamp': instance.timestamp?.map((e) => e.toIso8601String()).toList(),
     };
