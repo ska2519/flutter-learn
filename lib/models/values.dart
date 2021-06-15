d9419048f2ae835cef5b7233a5a01096d30c90a4
Ska Lee <ska2519@gmail.com>
2021-06-15 12:57:49 +0900

    Backup before timestamp update

--- lib/models/values.dart
+++ lib/models/values.dart
@@ -14,7 +14,21 @@
 
 import 'dart:math';
 
-final cities = [
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_learn/services/firestore_database.dart';
+
+final _ids = [
+  'a6hdJFD7qVfDe6yvRZF4SFwtP903',
+  'RPxVy0vtJYULUesWjL4ceXB1h3H2',
+  '7ytll7EosoUNI8Ix2hpPf8ZR3rH3',
+];
+final _userIds = [
+  'a6hdJFD7qVfDe6yvRZF4SFwtP903',
+  'RPxVy0vtJYULUesWjL4ceXB1h3H2',
+  '7ytll7EosoUNI8Ix2hpPf8ZR3rH3',
+];
+
+final _cities = [
   'Albuquerque',
   'Arlington',
   'Atlanta',
@@ -65,7 +79,7 @@ final cities = [
   'Washington',
 ];
 
-final categories = [
+final _categories = [
   'Brunch',
   'Burgers',
   'Coffee',
@@ -106,42 +120,42 @@ final _words = [
   'Eating',
 ];
 
-final _reviewTextPerRating = {
-  1: [
-    'Would never eat here again!',
-    'Such an awful place!',
-    'Not sure if they had a bad day off, but the food was very bad.'
-  ],
-  2: [
-    'Not my cup of tea.',
-    'Unlikely that we will ever come again.',
-    "Quite bad, but I've had worse!"
-  ],
-  3: [
-    'Exactly okay :/',
-    'Unimpressed, but not disappointed!',
-    '3 estrellas y van que arden.'
-  ],
-  4: [
-    'Actually pretty good, would recommend!',
-    'I really like this place, I come quite often!',
-    'A great experience, as usual!'
-  ],
-  5: [
-    'This is my favorite place. Literally',
-    'This place is ALWAYS great!',
-    'I recommend this to all my friends and family!'
-  ],
-};
+final _contents = [
+  'Would never eat here again!',
+  'Such an awful place!',
+  'Not sure if they had a bad day off, but the food was very bad.',
+  'Not my cup of tea.',
+  'Unlikely that we will ever come again.',
+  "Quite bad, but I've had worse!",
+  'Exactly okay :/',
+  'Unimpressed, but not disappointed!',
+  '3 estrellas y van que arden.',
+  'Actually pretty good, would recommend!',
+  'I really like this place, I come quite often!',
+  'A great experience, as usual!',
+  'This is my favorite place. Literally',
+  'This place is ALWAYS great!',
+  'I recommend this to all my friends and family!',
+];
 
 final random = Random();
 
-String getRandomReviewText(int rating) {
-  final reviews = _reviewTextPerRating[rating];
-  return reviews![random.nextInt(reviews.length)];
+String getRandomIds() {
+  final currentDate = documentIdFromCurrentDate();
+  print('currentDate: $currentDate');
+  final serverTimestamp = FieldValue.serverTimestamp();
+  print('serverTimestamp: $serverTimestamp');
+  // final now = DateTime.now();
+  // final timestamp = <DateTime>{now};
+  //${random.nextInt(currentDate)}:
+  return _ids[random.nextInt(_ids.length)];
 }
 
-String getRandomName() {
+String getRandomUserIds() {
+  return _userIds[random.nextInt(_userIds.length)];
+}
+
+String getRandomDisplayName() {
   final firstWord = random.nextInt(_words.length);
   int nextWord;
   do {
@@ -150,15 +164,16 @@ String getRandomName() {
   return '${_words[firstWord]} ${_words[nextWord]}';
 }
 
-String getRandomCategory() {
-  return categories[random.nextInt(categories.length)];
+String getRandomTitle() {
+  return _categories[random.nextInt(_categories.length)];
 }
 
-String getRandomCity() {
-  return cities[random.nextInt(cities.length)];
+String getRandomContent() {
+  return _contents[random.nextInt(_contents.length)];
 }
 
 String getRandomPhoto() {
   final photoId = random.nextInt(21) + 1;
-  return 'https://storage.googleapis.com/firestorequickstarts.appspot.com/food_$photoId.png';
+  return 'https://randomuser.me/api/portraits/women/$photoId.jpg';
+  // return 'https://storage.googleapis.com/firestorequickstarts.appspot.com/food_$photoId.png';
 }
