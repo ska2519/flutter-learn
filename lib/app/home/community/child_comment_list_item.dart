7da52d7eac915caf93c9d2ab78c61fe2de0527f6
Ska Lee <ska2519@gmail.com>
2021-06-23 01:44:01 +0900

    Update nested commets system

--- lib/app/home/community/child_comment_list_item.dart
+++ lib/app/home/community/child_comment_list_item.dart
@@ -6,15 +6,27 @@ import 'package:flutter_learn/models/comment.dart';
 import 'package:flutter_learn/services/firestore_database.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import 'comment_list_item.dart';
+
 final childCommentsStreamProvider =
     StreamProvider.autoDispose.family<List<Comment>, Comment>((ref, comment) {
   final database = ref.watch(databaseProvider);
   return database.childCommentsStream(comment);
 });
+typedef Callback = Future<void> Function(Comment childComment);
 
 class ChildCommentListItem extends HookWidget {
-  const ChildCommentListItem(this.comment, {Key? key}) : super(key: key);
+  const ChildCommentListItem({
+    required this.comment,
+    required this.menuIconTap,
+    required this.replyIconTap,
+    required this.callBack,
+    Key? key,
+  }) : super(key: key);
   final Comment comment;
+  final VoidCallback menuIconTap;
+  final VoidCallback replyIconTap;
+  final Callback callBack;
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +45,14 @@ class ChildCommentListItem extends HookWidget {
           shrinkWrap: true,
           itemCount: childComments.length,
           itemBuilder: (context, i) {
-            return Text(childComments[i].text);
+            return FutureBuilder(
+              future: callBack(childComments[i]),
+              builder: (context, snapshot) => CommentListItem(
+                comment: childComments[i],
+                menuIconTap: menuIconTap,
+                replyIconTap: replyIconTap,
+              ),
+            );
           },
         );
       },
