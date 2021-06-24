737ce084a2cb82f3487b1c76660cf2cb7a645b1a
Ska Lee <ska2519@gmail.com>
2021-06-24 19:16:29 +0900

    fix git commit

--- functions/src/firestore.ts
+++ functions/src/firestore.ts
@@ -1,26 +1,33 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 admin.initializeApp();
-// const db = admin.firestore();
+const db = admin.firestore();
 
-// 딜레이 걸려서 많은 데이터 입력 시 모두 안들어감 - 사용 불가
-// export const increaseCommentCount = functions.firestore.document('posts/{postId}/comments/{commentId}').onCreate(async (snap, __) => {
-//     const postId = snap.get('postId');
-//     const postRef = db.doc('posts/' + postId);
-//     const postSnap = await postRef.get();
-//     const postData = postSnap.data();
-//     return await postRef.update({commentCount: postData?.commentCount + 1});
-// });
+// const postSnap = await postRef.get();
+// const postData = postSnap.data();
+// await postRef.update({commentCount: postData?.commentCount + 1});
 
-// export const decreaseCommentCount = functions.firestore.document('posts/{postId}/comments/{commentId}').onDelete(async (snap, __) => {
-//     const postId = snap.get('postId');
-//     const postRef = db.doc('posts/' + postId);
-//     const postSnap = await postRef.get();
-//     const postData = postSnap.data();
-//     return await postRef.update({commentCount: postData?.commentCount - 1});
-// });
+export const increaseCommentCount = functions.firestore.document('posts/{postId}/comments/{commentId}').onCreate(async (snap, __) => {
+    const postId = snap.get('postId');
+    const postRef = db.doc('posts/' + postId);
+    try {
+        await postRef.update({commentCount: admin.firestore.FieldValue.increment(1)});
+    } catch (error) {
+        console.log('failed increaseCommentCount');
+    }
+});
 
-export const addPostId = functions.firestore
-    .document('posts/{postId}')
-    .onCreate(async (snap, _) => await snap.ref.update({id: snap.id}));
+export const decreaseCommentCount = functions.firestore.document('posts/{postId}/comments/{commentId}').onDelete(async (snap, __) => {
+    const postId = snap.get('postId');
+    const postRef = db.doc('posts/' + postId);
+    try {
+        await postRef.update({commentCount: admin.firestore.FieldValue.increment(-1)});
+    } catch (error) {
+        console.log('failed decreaseCommentCount');
+    }
+});
+
+// export const addPostId = functions.firestore
+//     .document('posts/{postId}')
+//     .onCreate(async (snap, _) => await snap.ref.update({id: snap.id}));
 
--- lib/services/firestore_path.dart
+++ lib/services/firestore_path.dart
@@ -2,11 +2,12 @@ class FirestorePath {
   static String appUser(String id) => 'users/$id';
   static String post(String postId) => 'posts/$postId';
   static String posts() => 'posts';
+  static String comment(String postId, String commentId) =>
+      '/posts/$postId/comments/$commentId';
   static String comments(String postId) => '/posts/$postId/comments';
   static String postLiked(String postId) => '/posts/$postId/likedUsers';
   static String collectionGroupComments() => 'comments';
-  static String comment(String postId, String commentId) =>
-      '/posts/$postId/comments/$commentId';
+
   static String postReadUsers(String postId) => 'posts/$postId/readUsers';
   static String postReadUser(String postId, String userId) =>
       'posts/$postId/readUsers/$userId';
