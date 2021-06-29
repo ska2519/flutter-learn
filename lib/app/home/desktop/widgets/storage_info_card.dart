3caf70d860ecab163f61f02f74b80f7bee4319c7
Ska Lee <ska2519@gmail.com>
2021-06-29 16:34:05 +0900

    Update AccountPage with Much Things

--- lib/app/home/desktop/widgets/storage_info_card.dart
+++ lib/app/home/desktop/widgets/storage_info_card.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
 import 'package:flutter_learn/constants/constants.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class StorageInfoCard extends StatelessWidget {
   const StorageInfoCard({
--- lib/models/my_files.dart
+++ lib/models/my_files.dart
@@ -1,5 +1,6 @@
-import 'package:flutter_learn/constants/constants.dart';
 import 'package:flutter/material.dart';
+
+import 'package:flutter_learn/constants/constants.dart';
 
 class CloudStorageInfo {
   final String svgSrc, title, totalStorage;
