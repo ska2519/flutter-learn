ddcdcbb6acb93b196617d628819c47b78c8efc9c
Ska Lee <ska2519@gmail.com>
2021-07-03 17:57:52 +0900

    Search Process update

--- lib/secret_keys.dart
+++ lib/secret_keys.dart
@@ -1,2 +1,4 @@
 const releaseAdminUid = '8iWpn2anXhYgnU5EoIFIl4ypU0c2';
 const debugAdminUid = '9VoOQ2nwiMdVJSQeFFnnKAUkwKV2';
+const algoliaApplicationID = 'BNN5IG0AGT';
+const algoliaApiKey = 'c5f062ad6443d36bc63b14820208ec67';
