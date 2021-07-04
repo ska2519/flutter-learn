85969e9652b5152326120b855740918c4e523e67
Ska Lee <ska2519@gmail.com>
2021-07-04 17:54:14 +0900

    Update private post & comment progress

--- lib/secret_keys.dart
+++ lib/secret_keys.dart
@@ -1,4 +1,5 @@
 const releaseAdminUid = '8iWpn2anXhYgnU5EoIFIl4ypU0c2';
-const debugAdminUid = '9VoOQ2nwiMdVJSQeFFnnKAUkwKV2';
+const debugAdminUid = 'hcuJRhh6nhcFk6ktPiKymUezcWl1';
+
 const algoliaApplicationID = 'BNN5IG0AGT';
 const algoliaApiKey = 'c5f062ad6443d36bc63b14820208ec67';
