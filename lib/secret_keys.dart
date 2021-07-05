0c2bde45a8d612b74d5b9fcb37e2dec5547d0b4f
Ska Lee <ska2519@gmail.com>
2021-07-05 12:43:59 +0900

    SearchPage separate release  & debug index

--- lib/secret_keys.dart
+++ lib/secret_keys.dart
@@ -2,4 +2,5 @@ const releaseAdminUid = '8iWpn2anXhYgnU5EoIFIl4ypU0c2';
 const debugAdminUid = 'hcuJRhh6nhcFk6ktPiKymUezcWl1';
 
 const algoliaApplicationID = 'BNN5IG0AGT';
-const algoliaApiKey = 'c5f062ad6443d36bc63b14820208ec67';
+const algoliaDebugApiKey = 'c5f062ad6443d36bc63b14820208ec67';
+const algoliaReleaseApiKey = 'c59d727d308f43f0561f7c0a7beb2ad9';
