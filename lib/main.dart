207b0f227bd2c96710158ea5f47e25ca136fef6e
Ska Lee <ska2519@gmail.com>
2021-07-12 20:59:43 +0900

    update LicenseSimplePage

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -19,6 +20,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   await Firebase.initializeApp();
+
   runApp(ProviderScope(
     child: EasyLocalization(
       supportedLocales: [Locale('en'), Locale('ko')],
